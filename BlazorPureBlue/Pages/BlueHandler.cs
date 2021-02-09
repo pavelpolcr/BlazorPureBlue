a6ca297e55d50f6afab9e894f07093969a74bcd5
Pavel Polcr <pavel.polcr@seznam.cz>
2021-02-09 10:24:30 +0100

    Add project files.

---
+++ BlazorPureBlue/Pages/BlueHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorPureBlue
+{
+    public class BlueHandler
+    {
+        public BlueHandler()
+        {
+            DayTaskRecords = new List<DayTaskRecord>();
+            TaskTitles = new List<string>();
+        }
+
+        public List<DayTaskRecord> DayTaskRecords { get; set; }
+
+        public List<string> TaskTitles { get; set; }
+    }
+    public class DayTaskRecord
+    {
+        public DateTime Date { get; set; }
+        public int TaskIndex { get; set; }
+    }
+}
---
+++ BlazorPureBlue/Services/BHService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorPureBlue
+{
+    public static class BHService
+    {
+        private static BlueHandler bh;
+
+        public static BlueHandler BH
+        {
+            get { return bh; }
+            set { bh = value; }
+        }
+
+    }
+}
